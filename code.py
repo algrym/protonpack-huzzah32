6113923a142506380a0d54d3813b6a7468ea7086
A. J. Wright <ajw@toadking.org>
2023-09-10 19:41:23 -0400

    Working on library setup

--- code.py
+++ code.py
@@ -6,16 +6,21 @@
 import time
 import board
 import neopixel
+import version
+import adafruit_fancyled.adafruit_fancyled as fancyled
 
 led = neopixel.NeoPixel(board.NEOPIXEL, 1)
 
 led.brightness = 0.3
 
+print("github.com/algrym/protonpack-huzzah32/code.py -", version.__version__)
+print(f" - Adafruit FancyLed v{fancyled.__version__}")
+
 while True:
+    print("Hello World!")
     led[0] = (255, 0, 0)
     time.sleep(0.5)
     led[0] = (0, 255, 0)
     time.sleep(0.5)
     led[0] = (0, 0, 255)
     time.sleep(0.5)
-print("Hello World!")
