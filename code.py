34d5156e6c82b0f38184150e6940659afecd8ad8
A. J. Wright <ajw@toadking.org>
2023-09-10 19:10:02 -0400

    Imported default code.py

---
+++ code.py
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""CircuitPython Essentials Internal RGB LED red, green, blue example"""
+import time
+import board
+import neopixel
+
+led = neopixel.NeoPixel(board.NEOPIXEL, 1)
+
+led.brightness = 0.3
+
+while True:
+    led[0] = (255, 0, 0)
+    time.sleep(0.5)
+    led[0] = (0, 255, 0)
+    time.sleep(0.5)
+    led[0] = (0, 0, 255)
+    time.sleep(0.5)
+print("Hello World!")
